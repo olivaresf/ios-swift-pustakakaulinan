503d37482ca2277ef5c22cca6867214ac3e86eb8
Prima Santosa <mauliawan@primasantosa.com>
2020-07-11 13:11:12 +0700

    Initial Commit

---
+++ PustakaKaulinan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PustakaKaulinan
+//
+//  Created by comprimasantosa on 11/07/20.
+//  Copyright © 2020 comprimasantosa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
